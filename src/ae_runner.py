2abad4ec170bc9007b305b79309eb7ec33588633
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-06-20 22:09:39 -0700

    simplified

--- src/ae_runner.py
+++ src/ae_runner.py
@@ -1,29 +1,23 @@
 import argparse
 
 import torch.utils.data
-from torchvision import datasets, transforms
 
 from autoencoder import *
+from utils import cifar10_util
 
 
 # Referred to https://github.com/wanglouis49/pytorch-autoencoders
 def get_argparser():
     parser = argparse.ArgumentParser(description='VAE CIFAR-10')
     parser.add_argument('-data', default='./resource/data/', help='CIFAR-10 data dir path')
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                        help='input batch size for training (default: 128)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='enables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    parser.add_argument('-epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
+    parser.add_argument('-no_cuda', action='store_true', default=False, help='enables CUDA training')
+    parser.add_argument('-seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+    parser.add_argument('-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--hidden-size', type=int, default=20, metavar='N',
-                        help='how big is z')
-    parser.add_argument('--intermediate-size', type=int, default=128, metavar='N',
-                        help='how big is linear around z')
+    parser.add_argument('-hsize', type=int, default=20, metavar='N', help='how big is z')
+    parser.add_argument('-vrate', type=float, default=0.1, help='validation rate')
+    parser.add_argument('-inter_size', type=int, default=128, metavar='N', help='how big is linear around z')
     return parser
 
 
@@ -38,7 +32,7 @@ def train(model, train_loader, optimizer, epoch, args):
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % args.interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss / len(data)))
@@ -66,16 +60,10 @@ def run(args):
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
 
-    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10(data_dir_path, train=True, download=True,
-                         transform=transforms.ToTensor()),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10(data_dir_path, train=False, transform=transforms.ToTensor()),
-        batch_size=args.batch_size, shuffle=False, **kwargs)
+    train_loader, valid_loader, test_loader =\
+        cifar10_util.get_data_loaders(data_dir_path, args.vrate)
 
-    model = VAE(args.intermediate_size, args.hidden_size)
+    model = VAE(args.inter_size, args.hsize)
     if args.cuda:
         model.cuda()
 
