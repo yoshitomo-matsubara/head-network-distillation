695849e56b50c9b0984d7446b5a1746336d2c640
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-06-21 23:56:59 -0700

    forgot to call super

--- src/autoencoders/ae.py
+++ src/autoencoders/ae.py
@@ -2,6 +2,9 @@ import torch.nn as nn
 
 
 class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+
     def forward(self, x):
         raise NotImplementedError
 
