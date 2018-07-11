3c6c4c9a2d72ae65948c17a3f47f21859d39be82
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-07-10 23:08:22 -0700

    added caltech dataset util

---
+++ src/utils/caltech_util.py
@@ -0,0 +1,107 @@
+import os
+
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+from autoencoders import *
+from models.cifar import *
+from utils import data_util, file_util
+from utils.data_util import RgbImageDataset
+
+
+def get_model(device, config):
+    model_config = config['model']
+    model_type = model_config['type']
+    if model_type == 'alexnet':
+        model = AlexNet(**model_config['params'])
+    elif model_type == 'densenet':
+        model = DenseNet(**model_config['params'])
+    elif model_type == 'lenet5':
+        model = LeNet5(**model_config['params'])
+    elif model_type.startswith('resnet'):
+        model = resnet_model(model_type, model_config['params'])
+    else:
+        model = None
+    model = model.to(device)
+    if device == 'cuda':
+        model = torch.nn.DataParallel(model)
+    return model
+
+
+def get_autoencoder(cuda_available, config):
+    ae_config = config['autoencoder']
+    ae_type = ae_config['type']
+    if ae_type == 'vae':
+        ae = VAE(**ae_config['params'])
+    else:
+        ae = None
+
+    if cuda_available:
+        ae.cuda()
+    return ae
+
+
+def get_test_transformer(normalizer, compression_type, compressed_size_str, org_size=(180, 180), ae=None):
+    normal_list = [transforms.ToTensor()]
+    if ae is not None:
+        normal_list.append(AETransformer(ae))
+
+    if normalizer is not None:
+        normal_list.append(normalizer)
+
+    normal_transformer = transforms.Compose(normal_list)
+    if compression_type is None or compressed_size_str is None:
+        return normal_transformer
+
+    hw = compressed_size_str.split(',')
+    compressed_size = (int(hw[0]), int(hw[1]))
+    if compression_type == 'base':
+        comp_list = [transforms.Resize(compressed_size), transforms.Resize(org_size), transforms.ToTensor()]
+        if normalizer is not None:
+            comp_list.append(normalizer)
+        return transforms.Compose(comp_list)
+    return normal_transformer
+
+
+def get_data_loaders(data_dir_path, reshape_size=(180, 180), compression_type=None, compressed_size_str=None,
+                     valid_rate=0.1, test_rate=0.1, random_seed=1, normalized=True, is_caltech256=False, ae=None):
+    dataset_name = 'Caltech101' if not is_caltech256 else 'Caltech256'
+    if not os.path.exists(data_dir_path):
+        ValueError('Could not find %s dataset at %s' % dataset_name % data_dir_path)
+
+    sub_dir_path_list = file_util.get_dir_list(data_dir_path)
+    file_path_lists = []
+    for sub_dir_path in sub_dir_path_list:
+        file_path_lists.append(file_util.get_file_list(sub_dir_path))
+
+    train_file_path_lists, valid_file_path_lists, test_file_path_lists = [], [], []
+    np.random.seed(random_seed)
+    for file_path_list in file_path_lists:
+        np.random.shuffle(file_path_list)
+        sample_size = len(file_path_list)
+        train_end_idx = int(sample_size * (1 - valid_rate - test_rate))
+        valid_end_idx = train_end_idx + int(sample_size * valid_rate)
+        train_file_path_lists.append(file_path_list[:train_end_idx])
+        valid_file_path_lists.append(file_path_list[train_end_idx:valid_end_idx])
+        test_file_path_lists.append(file_path_list[valid_end_idx:])
+
+    train_dataset = RgbImageDataset(train_file_path_lists, reshape_size)
+    normalizer = data_util.build_normalizer(train_dataset.data) if normalized else None
+    valid_comp_list = [transforms.ToTensor()]
+    train_comp_list = [transforms.RandomHorizontalFlip(), transforms.ToTensor()]
+    if normalizer is not None:
+        valid_comp_list.append(normalizer)
+        train_comp_list.append(normalizer)
+
+    pin_memory = torch.cuda.is_available()
+    train_dataset = RgbImageDataset(train_file_path_lists, reshape_size, train_comp_list)
+    valid_dataset = RgbImageDataset(valid_file_path_lists, reshape_size, valid_comp_list)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=100, num_workers=2, pin_memory=pin_memory)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=100, num_workers=2, pin_memory=pin_memory)
+
+    test_transformer = get_test_transformer(normalizer, compression_type, compressed_size_str, ae=ae)
+    test_dataset = RgbImageDataset(test_file_path_lists, reshape_size, test_transformer)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False,
+                                              num_workers=2, pin_memory=pin_memory)
+    return train_loader, valid_loader, test_loader
