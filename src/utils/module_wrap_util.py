8068d3bf4b503ed0f6da3509a48af654947b2a39
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-07-23 21:35:27 -0700

    fixed bugs

--- src/utils/module_wrap_util.py
+++ src/utils/module_wrap_util.py
@@ -1,17 +1,6 @@
 import torch.nn as nn
 
 
-def wrap_all_child_modules(model, wrapper_module, member_name=None, member_module=None):
-    named_children = model.named_children() if member_module is None else member_module.named_children()
-    named_children = list(named_children)
-    if not named_children and member_name is not None and member_module is not None:
-        setattr(model, member_name, wrapper_module(member_module))
-        return
-
-    for name, child_module in named_children:
-        wrap_all_child_modules(model, wrapper_module, name, child_module)
-
-
 class WrapperModule(nn.Module):
     def __init__(self, org_module):
         super().__init__()
@@ -20,3 +9,15 @@ class WrapperModule(nn.Module):
     def forward(self, *input):
         output = self.org_module(*input)
         return output
+
+
+def wrap_all_child_modules(model, wrapper_module, member_name=None, member_module=None):
+    named_children = model.named_children() if member_module is None else member_module.named_children()
+    named_children = list(named_children)
+    if not named_children and member_name is not None and member_module is not None:
+        setattr(model, member_name, wrapper_module(member_module))
+        return
+
+    parent = model if member_module is None else member_module
+    for name, child_module in named_children:
+        wrap_all_child_modules(parent, wrapper_module, name, child_module)
