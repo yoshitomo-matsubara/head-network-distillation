a36fd22ff9cf18ef76c7049247cc3d96a0516ab1
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-10-09 16:41:30 -0700

    refactored and added a function to extract all child modules

--- src/utils/module_util.py
+++ src/utils/module_util.py
@@ -14,7 +14,8 @@ def get_model(device, config):
     elif model_type.startswith('resnet'):
         model = resnet_model(model_type, model_config['params'])
     else:
-        model = None
+        ValueError('model_type `{}` is not expected'.format(model_type))
+
     model = model.to(device)
     if device == 'cuda':
         model = torch.nn.DataParallel(model)
@@ -27,8 +28,18 @@ def get_autoencoder(cuda_available, config):
     if ae_type == 'vae':
         ae = VAE(**ae_config['params'])
     else:
-        ae = None
+        ValueError('ae_type `{}` is not expected'.format(ae_type))
 
     if cuda_available:
         ae.cuda()
     return ae
+
+
+def extract_all_child_modules(parent_module, module_list):
+    child_models = list(parent_module.children())
+    if not child_models:
+        module_list.append(parent_module)
+        return
+
+    for child_module in child_models:
+        extract_all_child_modules(child_module, parent_module)
