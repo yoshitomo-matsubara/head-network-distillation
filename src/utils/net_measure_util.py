fixed unintentional scaling part

src/utils/net_measure_util.py
@@ -83,7 +83,7 @@ def plot_accumulated_model_complexity_and_bandwidth(xs, accumulated_op_counts, b
         tick.set_rotation(90)
 
     ax2 = ax1.twinx()
-    ax2.plot(xs[1:], accumulated_op_counts / np.max(accumulated_op_counts), 'r--')
+    ax2.plot(xs[1:], accumulated_op_counts, 'r--')
     ax2.set_ylabel(accum_complexity_label, color='r')
 
     plt.tight_layout()