daedd2304491588ae183fa4e08e6b7fdd38e9e4c
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-10-28 20:04:55 -0700

    updated ver.3

--- src/models/mimic/densenet_mimic.py
+++ src/models/mimic/densenet_mimic.py
@@ -26,13 +26,16 @@ def mimic_version3():
     return nn.Sequential(
         nn.BatchNorm2d(64),
         nn.ReLU(inplace=True),
-        nn.Conv2d(64, 128, kernel_size=2, stride=2, bias=False),
+        nn.Conv2d(64, 128, kernel_size=2, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(128),
         nn.ReLU(inplace=True),
-        nn.Conv2d(128, 256, kernel_size=2, stride=2, bias=False),
+        nn.Conv2d(128, 256, kernel_size=2, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(256),
         nn.ReLU(inplace=True),
-        nn.Conv2d(256, 640, kernel_size=2, stride=1, padding=1, bias=False),
+        nn.Conv2d(256, 512, kernel_size=2, stride=1, padding=1, bias=False),
+        nn.BatchNorm2d(512),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(512, 640, kernel_size=2, stride=1, bias=False),
         nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
     )
 
