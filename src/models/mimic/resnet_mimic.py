3319c3f7d76e35f06bfbc99b23dee72b09f99a21
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-10-30 14:19:15 -0700

    updated mimic ver.3

--- src/models/mimic/resnet_mimic.py
+++ src/models/mimic/resnet_mimic.py
@@ -40,14 +40,14 @@ def mimic_version3():
         nn.Conv2d(128, 256, kernel_size=2, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(256),
         nn.ReLU(inplace=True),
-        nn.Conv2d(256, 512, kernel_size=2, stride=1, padding=1, bias=False),
+        nn.Conv2d(256, 512, kernel_size=2, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(512),
         nn.ReLU(inplace=True),
         nn.Conv2d(512, 1024, kernel_size=2, stride=1, bias=False),
         nn.BatchNorm2d(1024),
         nn.ReLU(inplace=True),
         nn.Conv2d(1024, 2048, kernel_size=2, stride=1, bias=False),
-        nn.AvgPool2d(kernel_size=2, stride=2)
+        nn.AvgPool2d(kernel_size=7, stride=1)
     )
 
 
