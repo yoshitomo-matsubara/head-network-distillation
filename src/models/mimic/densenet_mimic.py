8f7e907034fd58a3c55b32e091d94fc8903827d4
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-11-05 22:01:15 -0800

    added bottleneck to mimic models

--- src/models/mimic/densenet_mimic.py
+++ src/models/mimic/densenet_mimic.py
@@ -15,18 +15,35 @@ def mimic_version1(make_bottleneck=False):
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 128, kernel_size=2, stride=1, bias=False),
-            nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+            nn.AvgPool2d(kernel_size=2, stride=2)
         )
 
     return nn.Sequential(
         nn.BatchNorm2d(64),
         nn.ReLU(inplace=True),
         nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1, bias=False),
-        nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+        nn.AvgPool2d(kernel_size=2, stride=2)
     )
 
 
-def mimic_version2():
+def mimic_version2(make_bottleneck=False):
+    if make_bottleneck:
+        return nn.Sequential(
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 3, kernel_size=2, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(3),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(3, 64, kernel_size=2, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=2, stride=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, kernel_size=1, stride=1, bias=False),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
     return nn.Sequential(
         nn.BatchNorm2d(64),
         nn.ReLU(inplace=True),
@@ -34,12 +51,35 @@ def mimic_version2():
         nn.BatchNorm2d(128),
         nn.ReLU(inplace=True),
         nn.Conv2d(128, 256, kernel_size=1, stride=1, bias=False),
-        nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+        nn.AvgPool2d(kernel_size=2, stride=2)
     )
 
 
-def mimic_version3(teacher_model_type):
+def mimic_version3(teacher_model_type, make_bottleneck=False):
     if teacher_model_type == 'densenet169':
+        if make_bottleneck:
+            return nn.Sequential(
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 3, kernel_size=2, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(3),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(3, 64, kernel_size=2, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(128),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(128, 256, kernel_size=2, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 512, kernel_size=2, stride=1, bias=False),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(512, 640, kernel_size=2, stride=1, bias=False),
+                nn.AvgPool2d(kernel_size=2, stride=2)
+            )
+
         return nn.Sequential(
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
@@ -53,9 +93,31 @@ def mimic_version3(teacher_model_type):
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 640, kernel_size=2, stride=1, bias=False),
-            nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+            nn.AvgPool2d(kernel_size=2, stride=2)
         )
     elif teacher_model_type == 'densenet201':
+        if make_bottleneck:
+            return nn.Sequential(
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 3, kernel_size=2, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(3),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(3, 64, kernel_size=2, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(128),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(128, 256, kernel_size=2, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 512, kernel_size=2, stride=1, bias=False),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(512, 896, kernel_size=2, stride=1, bias=False),
+                nn.AvgPool2d(kernel_size=2, stride=2)
+            )
         return nn.Sequential(
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
@@ -69,14 +131,14 @@ def mimic_version3(teacher_model_type):
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 896, kernel_size=2, stride=1, bias=False),
-            nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+            nn.AvgPool2d(kernel_size=2, stride=2)
         )
     raise ValueError('teacher_model_type `{}` is not expected'.format(teacher_model_type))
 
 
 class DenseNetHeadMimic(BaseHeadMimic):
     # designed for input image size [3, 224, 224]
-    def __init__(self, teacher_model_type, version, bottlenecked=False):
+    def __init__(self, teacher_model_type, version):
         super().__init__()
         self.extractor = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
@@ -84,12 +146,12 @@ class DenseNetHeadMimic(BaseHeadMimic):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
-        if version == 1:
-            self.module_seq = mimic_version1(bottlenecked)
-        elif version == 2:
-            self.module_seq = mimic_version2()
-        elif version == 3:
-            self.module_seq = mimic_version3(teacher_model_type)
+        if version in ['1' or '1b']:
+            self.module_seq = mimic_version1(version == '1b')
+        elif version in ['2' or '2b']:
+            self.module_seq = mimic_version2(version == '2b')
+        elif version in ['3' or '3b']:
+            self.module_seq = mimic_version3(teacher_model_type, version == '3b')
         else:
             raise ValueError('version `{}` is not expected'.format(version))
         self.initialize_weights()
