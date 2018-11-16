e6dc63b73cfad40953854153860db2f1421934ce
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-11-16 01:29:07 -0800

    modified to try to make smaller bottleneck

--- src/models/mimic/densenet_mimic.py
+++ src/models/mimic/densenet_mimic.py
@@ -36,10 +36,16 @@ def mimic_version2(make_bottleneck=False):
             nn.Conv2d(3, 64, kernel_size=2, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 128, kernel_size=2, stride=1, bias=False),
+            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 256, kernel_size=2, stride=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 512, kernel_size=2, stride=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, kernel_size=2, stride=1, bias=False),
             nn.AvgPool2d(kernel_size=2, stride=2)
         )
     return nn.Sequential(
