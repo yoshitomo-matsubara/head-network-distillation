7498a754fd49ac90c934baf305b84d2b33b2f74f
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-03-08 12:38:36 -0800

    added MobileNet vers.1 and 2

---
+++ src/models/classification/mobilenet.py
@@ -0,0 +1,130 @@
+# Referred to https://github.com/marvis/pytorch-mobilenet and https://github.com/kougou/pytorch-cifar
+import torch.nn as nn
+
+
+def conv_bn(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU(inplace=True)
+    )
+
+
+def conv_dw(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+        nn.BatchNorm2d(inp),
+        nn.ReLU(inplace=True),
+
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU(inplace=True),
+    )
+
+
+class MobileNetV1(nn.Module):
+    def __init__(self, num_classes=1000):
+        super().__init__()
+        self.seq = nn.Sequential(
+            conv_bn(3, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AvgPool2d(7),
+        )
+        self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.seq(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, in_planes, out_planes, expansion, stride):
+        super().__init__()
+        self.stride = stride
+        planes = expansion * in_planes
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_planes, planes, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(),
+            nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, groups=planes, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(),
+            nn.Conv2d(planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(out_planes)
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride == 1 and in_planes != out_planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(out_planes),
+            )
+
+    def forward(self, x):
+        z = self.seq(x)
+        return z + self.shortcut(x) if self.stride == 1 else z
+
+
+class MobileNetV2(nn.Module):
+    # (expansion, out_planes, num_blocks, stride)
+    cfg = [(1,  16, 1, 1),
+           (6,  24, 2, 2),
+           (6,  32, 3, 2),
+           (6,  64, 4, 2),
+           (6,  96, 3, 1),
+           (6, 160, 3, 2),
+           (6, 320, 1, 1)]
+
+    def __init__(self, num_classes=1000):
+        super().__init__()
+        self.seq1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.layers = self._make_layers(in_planes=32)
+        self.seq2 = nn.Sequential(
+            nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1280),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=7)
+        )
+        self.linear = nn.Linear(1280, num_classes)
+
+    def _make_layers(self, in_planes):
+        layers = []
+        for expansion, out_planes, num_blocks, first_stride in self.cfg:
+            strides = [first_stride] + [1]*(num_blocks-1)
+            for stride in strides:
+                layers.append(Block(in_planes, out_planes, expansion, stride))
+                in_planes = out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        z = self.seq1(x)
+        z = self.layers(z)
+        z = self.seq2(z)
+        z = z.view(z.size(0), -1)
+        return self.linear(z)
+
+
+def mobilenet_model(model_type, param_config):
+    if model_type == 'mobilenet_v1':
+        return MobileNetV1(param_config)
+    elif model_type == 'mobilenet_v2':
+        return MobileNetV2(param_config)
+    return None
