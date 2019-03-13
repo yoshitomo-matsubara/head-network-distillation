d61d4fcedf4fb8c9c39cd39bb25ff790fdc2395d
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-03-13 12:49:07 -0700

    added pretrained option for Inception v3

--- src/utils/module_util.py
+++ src/utils/module_util.py
@@ -19,7 +19,7 @@ def get_model(config, device=None):
     elif model_type.startswith('mobilenet'):
         model = mobilenet_model(model_type, model_config['params'])
     elif model_type.startswith('inception_v3'):
-        model = inception_v3(**model_config['params'])
+        model = inception_v3(model_config['pretrained'], **model_config['params'])
     elif model_type in torchvision.models.__dict__:
         model = torchvision.models.__dict__[model_type](**model_config['params'])
 
