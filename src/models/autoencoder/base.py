e21ef6d9cd87d8d789ecfd904520a4426258d3f6
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-06-15 11:11:46 -0700

    added prototype of autoencoder, its trainer and config

---
+++ src/models/autoencoder/base.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+
+class BaseAutoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, sample_batch):
+        raise NotImplementedError
