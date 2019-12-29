a43a86e916cddf20e11fdb4586df7930e1492abc
Yoshitomo Matsubara <yoshitom@uci.edu>
2019-12-29 09:48:59 -0800

    fixed a bug

--- src/mimic_runner.py
+++ src/mimic_runner.py
@@ -192,7 +192,9 @@ def run(args):
 
     org_model, teacher_model_type = mimic_util.get_org_model(teacher_model_config, device)
     if not args.student_only:
-        evaluate(org_model.to(device), test_loader, device, title='[Original model]')
+        if distributed:
+            org_model = DistributedDataParallel(org_model, device_ids=device_ids)
+        evaluate(org_model, test_loader, device, title='[Original model]')
 
     mimic_model = mimic_util.get_mimic_model(config, org_model, teacher_model_type, teacher_model_config, device)
     mimic_model_without_dp = mimic_model.module if isinstance(mimic_model, DataParallel) else mimic_model
