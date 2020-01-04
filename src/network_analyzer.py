7444b9ef4fc15fa8bc8f0b73ed20ea878ab565c4
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-01-04 14:10:07 -0800

    simplified

--- src/network_analyzer.py
+++ src/network_analyzer.py
@@ -51,16 +51,17 @@ def analyze(model, input_shape, model_type, scaled, submoduled, plot):
 
 
 def analyze_single_model(config_file_path, args, plot=True):
+    cpu_device = torch.device('cpu')
     if file_util.check_if_exists(config_file_path):
         config = yaml_util.load_yaml_file(config_file_path)
         if 'teacher_model' in config:
             teacher_model_config = config['teacher_model']
-            org_model, teacher_model_type = mimic_util.get_org_model(teacher_model_config, 'cpu')
-            model = mimic_util.get_mimic_model(config, org_model, teacher_model_type, teacher_model_config, 'cpu')
+            org_model, teacher_model_type = mimic_util.get_org_model(teacher_model_config, cpu_device)
+            model = mimic_util.get_mimic_model(config, org_model, teacher_model_type, teacher_model_config, cpu_device)
             model_type = config['mimic_model']['type']
             input_shape = config['input_shape']
         elif 'autoencoder' in config:
-            model, model_type = ae_util.get_autoencoder(config, 'cpu', True)
+            model, model_type = ae_util.get_autoencoder(config, cpu_device, True)
             input_shape = config['input_shape']
         else:
             model, model_type, input_shape = read_config(config_file_path)
@@ -94,7 +95,8 @@ def analyze_multiple_models(config_file_paths, args):
 
 def get_teacher_and_student_models(mimic_config, input_shape):
     teacher_model_config = mimic_config['teacher_model']
-    teacher_model, teacher_model_type = mimic_util.get_teacher_model(teacher_model_config, input_shape, 'cpu')
+    teacher_model, teacher_model_type =\
+        mimic_util.get_teacher_model(teacher_model_config, input_shape, torch.device('cpu'))
     student_model = mimic_util.get_student_model(teacher_model_type, mimic_config['student_model'])
     return teacher_model_type, teacher_model, student_model
 
