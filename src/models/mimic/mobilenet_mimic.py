eb39bf1d4ca033c32f2c1cccaee7d88ee5e8fc43
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-01-07 00:16:50 -0800

    simplified

--- src/models/mimic/mobilenet_mimic.py
+++ src/models/mimic/mobilenet_mimic.py
@@ -90,22 +90,23 @@ def mimic_version2(make_bottleneck, bottleneck_channel):
 
 
 def mimic_version_test1(bottleneck_channel):
+    # end_idx: 27
     return nn.Sequential(
         nn.BatchNorm2d(64),
         nn.ReLU(inplace=True),
-        nn.Conv2d(64, bottleneck_channel, kernel_size=2, stride=2, padding=1, bias=False),
+        nn.Conv2d(64, bottleneck_channel, kernel_size=2, stride=1, padding=1, bias=False),
         nn.BatchNorm2d(bottleneck_channel),
         nn.ReLU(inplace=True),
-        nn.Conv2d(bottleneck_channel, 512, kernel_size=2, stride=1, padding=1, bias=False),
-        nn.BatchNorm2d(512),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(512, 512, kernel_size=2, stride=1, padding=1, bias=False),
-        nn.BatchNorm2d(512),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(512, 256, kernel_size=2, stride=1, bias=False),
+        nn.Conv2d(bottleneck_channel, 256, kernel_size=2, stride=1, padding=1, bias=False),
         nn.BatchNorm2d(256),
         nn.ReLU(inplace=True),
-        nn.Conv2d(256, 192, kernel_size=2, stride=1, bias=False),
+        nn.Conv2d(256, 128, kernel_size=2, stride=1, padding=1, bias=False),
+        nn.BatchNorm2d(128),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(128, 64, kernel_size=2, stride=1, bias=False),
+        nn.BatchNorm2d(64),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(64, 32, kernel_size=2, stride=1, bias=False),
         nn.AvgPool2d(kernel_size=2, stride=2)
     )
 
