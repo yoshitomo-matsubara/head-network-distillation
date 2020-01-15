8c0cbd7d2bfc09888aba91f525ea7d2cf7bb7097
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-01-15 00:15:57 -0800

    tried groups parameter

--- src/models/mimic/mobilenet_mimic.py
+++ src/models/mimic/mobilenet_mimic.py
@@ -13,13 +13,13 @@ def mimic_version1b_with_aux(bottleneck_channel, aux_output_size=1000):
         nn.ConvTranspose2d(bottleneck_channel, 512, kernel_size=4, stride=2, bias=False),
         nn.BatchNorm2d(512),
         nn.ReLU6(inplace=True),
-        nn.Conv2d(512, 256, kernel_size=2, stride=1, bias=False),
+        nn.Conv2d(512, 256, kernel_size=2, stride=1, groups=256, bias=False),
         nn.BatchNorm2d(256),
         nn.ReLU6(inplace=True),
-        nn.Conv2d(256, 32, kernel_size=2, stride=1, bias=False),
+        nn.Conv2d(256, 32, kernel_size=2, stride=1, groups=32, bias=False),
         nn.BatchNorm2d(32),
         nn.ReLU6(inplace=True),
-        nn.Conv2d(32, 32, kernel_size=2, stride=1, bias=False),
+        nn.Conv2d(32, 32, kernel_size=2, stride=1, groups=32, bias=False),
         nn.AvgPool2d(kernel_size=2, stride=2)
     ]
     return SeqWithAux(modules, aux_idx=2, aux_input_channel=bottleneck_channel, aux_output_size=aux_output_size)
